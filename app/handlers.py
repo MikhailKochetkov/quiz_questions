614b0d5d9b43aa3071e49936f555bc9048d19fbc
MikhailKochetkov <consultant.dax@gmail.com>
2023-05-26 14:21:30 +0300

    Refactoring

--- app/handlers.py
+++ app/handlers.py
@@ -1,7 +1,9 @@
 import requests
-from fastapi import APIRouter
-from db.models import connect_db
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
 from db.models import Quiz
+from db.session import get_db
 from .schemas import Question
 from settings import URL
 
@@ -14,36 +16,27 @@ def get_data():
 
 
 def get_quiz_questions(num_questions):
-    session = connect_db()
     quiz_questions = []
-    for i in range(num_questions):
-        while True:
-            data = get_data()
-            question_id = data["id"]
-            question = data["question"]
-            answer = data["answer"]
-            created_at = data["created_at"]
-            category_id = data["category_id"]
-            game_id = data["game_id"]
-            if not session.query(Quiz).filter_by(question_id=question_id).first():
-                break
+    for num in range(num_questions):
+        data = get_data()
         quiz_question = Quiz(
-            question_id=question_id,
-            question=question,
-            answer=answer,
-            created_at=created_at,
-            category_id=category_id,
-            game_id=game_id
+            question_id=data["id"],
+            question=data["question"],
+            answer=data["answer"],
+            created_at=data["created_at"],
+            category_id=data["category_id"],
+            game_id=data["game_id"]
         )
-        session.add(quiz_question)
-        session.commit()
-        session.refresh(quiz_question)
         quiz_questions.append(quiz_question)
-    session.close()
     return quiz_questions
 
 
 @router.post("/quiz")
-def create_quiz(question: Question):
+def create_quiz(question: Question, session: Session = Depends(get_db)):
     quiz_questions = get_quiz_questions(question.questions_num)
+    for quiz_question in quiz_questions:
+        session.add(quiz_question)
+        session.commit()
+        session.refresh(quiz_question)
+    session.close()
     return {"quiz_questions": quiz_questions}
