9ac9ce667369ad47b178e0b1cdd2121189fbc354
MikhailKochetkov <consultant.dax@gmail.com>
2023-06-04 22:52:33 +0300

    Some changes

--- db/db_connection.py
+++ db/db_connection.py
@@ -9,6 +9,6 @@ CONNECTION_STRING = (f'{os.getenv("DATABASE_URL", default="sqlite:///")}'
 PG_CONNECTION_STRING = (f'{os.getenv("PG_DATABASE_URL", default="postgresql://")}'
                         f'{os.getenv("DB_USER", default="postgres")}:'
                         f'{os.getenv("DB_PASSWORD", default="postgres")}@'
-                        f'{os.getenv("DB_HOST", default="127.0.0.1")}:'
+                        f'{os.getenv("DB_HOST", default="db")}:'
                         f'{os.getenv("DB_PORT", default=5432)}/'
                         f'{os.getenv("PG_DB_NAME", default="questions")}')
