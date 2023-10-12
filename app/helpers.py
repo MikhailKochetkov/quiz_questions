a290e07c82da28dabe1b7745cd9b1af9402c69ad
MikhailKochetkov <consultant.dax@gmail.com>
2023-10-12 21:06:18 +0300

    Helpers added

---
+++ app/helpers.py
@@ -0,0 +1,38 @@
+import requests
+
+from uuid import uuid4
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql import select
+
+from db.models import Quiz
+from db.session import get_session
+from settings import DESTINATION_URL
+
+
+def get_data() -> dict:
+    response = requests.get(DESTINATION_URL)
+    return response.json()[0]
+
+
+async def get_quiz_questions(
+        num_questions,
+        session: AsyncSession = Depends(get_session)) -> list:
+    quiz_questions = []
+    for num in range(num_questions):
+        while True:
+            data = get_data()
+            result = await session.execute(select(Quiz).filter_by(question_id=data['id']))
+            if not result.first():
+                break
+        quiz_question = Quiz(
+            id=uuid4(),
+            question_id=data['id'],
+            question=data['question'],
+            answer=data['answer'],
+            created_at=data['created_at'],
+            category_id=data['category_id'],
+            game_id=data['game_id']
+        )
+        quiz_questions.append(quiz_question)
+    return quiz_questions
