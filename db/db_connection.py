15479edffc7d33ee83c77db8fb024966502b1711
MikhailKochetkov <consultant.dax@gmail.com>
2023-10-13 10:48:47 +0300

    Refactoring

--- db/db_connection.py
+++ db/db_connection.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-CONNECTION_STRING = (f'{os.getenv("PG_DB_URL", default="postgresql")}'
+CONNECTION_STRING = (f'{os.getenv("PG_DB_URL", default="postgresql+asyncpg://")}'
                      f'{os.getenv("POSTGRES_USER", default="postgres")}:'
                      f'{os.getenv("POSTGRES_PASSWORD", default="postgres")}'
                      f'@'
-                     f'{os.getenv("DB_HOST", default="localhost")}:'
+                     f'{os.getenv("DB_HOST", default="db")}:'
                      f'{os.getenv("DB_PORT", default=5432)}/'
                      f'{os.getenv("POSTGRES_DB", default="quiz")}')
